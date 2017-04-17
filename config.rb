a3a0dddcab935093bfc5c15cc76f1eee4baeaa4e
Nolan Marketti <nolan@futuremandigital.com>
2017-04-16 21:16:03 -0500

    Update config

--- config.rb
+++ config.rb
@@ -52,8 +52,7 @@ configure :development do
 end
 
 # S3
-configure :staging do
-		activate :s3_sync do |s3_sync|
-		  s3_sync.bucket                     = 'staging.ryanfukuda.com' # The name of the S3 bucket you are targeting. This is globally unique.
-      s3_sync.region                     = 'us-east-1'     # The AWS region for your bucket.
+activate :s3_sync do |s3_sync|
+  s3_sync.bucket                = 'staging.ryanfukuda.com'
+  s3_sync.region                = 'us-east-1'
 end
